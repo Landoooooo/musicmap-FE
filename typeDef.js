9be6eb4f2d200f7445bb255edacf214c1456785b
Alando Appling <alandoappling@gmail.com>
2019-06-07 13:53:17 -0700

    Setting up initial GraphQL typeDefs

--- typeDef.js
+++ typeDef.js
@@ -1,5 +1,50 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
+    type Query {
+        #//*User
+        getCurrentUser: User
+        getUserBy(param: String!, value: String!): User
+        getUserById(userId: ID!): User
+    }
 
+    type Mutation {
+        #//*User
+        addUser(input: UserInput!): User!
+        updateUser(id: ID!, input: UserInput!): User!
+        deleteUser(id: ID!): Int!
+    }
+
+    scalar Date
+
+    type User {
+        id: ID!
+        email: String!
+        firstName: String!
+        lastName: String!
+        username: String!
+        location: String!
+        type: String!
+        profile_photo: String!
+    }
+
+    type Status {
+        id: ID!
+        user_id: User!
+        text: String!
+        photo: String!
+        video: String!
+        audio: String!
+    }
+
+    input UserInput {
+        email: String!
+        firstName: String!
+        lastName: String!
+        username: String!
+        location: String!
+        type: String!
+        profile_photo: String!
+
+    }
 `;
